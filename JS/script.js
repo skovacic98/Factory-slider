b07078edc04ff5930a985f98fb0f0de1079959e3
Silvio <silviokovacicofficial@gmail.com>
2021-07-14 11:44:57 +0200

    Ne znam kako bih napravio cijeli slideshow, odnosno da je infinite, što sam pokušavao nije išlo, vjerojatno bi se to trebalo na drugačiji način napraviti, animacije mi ne rade za prebacivanje slika, vjerujem da baš zbog tog načina prebacivanja slika kojeg sam odabrao, beskonačni viewport odnosno da slike idu van sa ekrana da ni ne spominjem, tu ne znam ni gdje bih krenuo..

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,25 @@
+function dohvatiSlike()
+{
+    return document.getElementsByClassName("my");
+}
+
+function promjeniLink(imageNumber)
+{
+    return "Assets/slider-image-"+imageNumber+".jpg";
+}
+
 function prebaciSliku(){
-    var firstImage = document.getElementsByClassName("pic1");
-    var image = document.getElementsByClassName("pic9");
-    console.log(image);
+    let images = dohvatiSlike();
+    imageNumber = 9;
+    for (let index = 8; index >= 0; index--) {
+        let link = promjeniLink(imageNumber-1);
+        imageNumber--;
+        if(index == 0)
+        {
+            imageNumber = 9;
+            link = promjeniLink(imageNumber);
+        }
+        images[index].src = link;
+        //console.log(images);
+    }
 }
